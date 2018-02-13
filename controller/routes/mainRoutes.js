56424fac66d85045013909ed43f00a06e6541425
Evan Tucker <eej.tucker@gmail.com>
2018-02-13 09:05:45 -0700

    added ejs

--- controller/routes/mainRoutes.js
+++ controller/routes/mainRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const ejs = require('ejs');
 
 router.get('/', (req, res, next)=> {
-    res.send('home page');
+    // res.send('home page');
+    res.render('./pages/home');
 });
 
 router.get('/info', (req, res, next)=> {
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+// dependencies
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const ejs = require('ejs');
 const main = require('./controller/routes/mainRoutes.js');
 
-// app.get('/', (req, res, next)=> {
-//     res.send('home page');
-// })
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
+app.set('view engine', 'ejs');
+
+// routes
 app.use('/', main);
 
 app.listen(port, ()=> {
     console.log(`app listening on port ${port}!`)
-})
+});
