d201004c85a67e4630e679d7844c5d3f6a42620f
Evan Tucker <eej.tucker@gmail.com>
2018-02-14 08:39:11 -0700

    added db connection

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const main = require('./controller/routes/mainRoutes.js');
 
 // middleware
@@ -14,6 +15,12 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 app.set('view engine', 'ejs');
+
+// connect to db
+mongoose.connect('mongodb://heroku_1fd77vjf:jql0tc7igk312bni82qn46shct@ds235768.mlab.com:35768/heroku_1fd77vjf', (err)=> {
+    if(err) throw err;
+    else console.log('successfully connected to db');
+});
 
 // routes
 app.use('/', main);
